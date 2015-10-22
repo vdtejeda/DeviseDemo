e2622a0746df3e8b20e1a1c71790a8768a0cf5e0
Volney Tejeda <volneytejeda10@gmail.com>
2015-10-21 22:02:08 -0400

    Add main menu and mailer

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise', '~> 3.4.1'
 
+gem 'delayed_job_active_record'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :lockable
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+ # If you are going to deploy the app on Heroku, create a Procfile in the root of your project and add
+ # worker: rake jobs:work
+  # one worker process to handle the jobs will cost you about $35 per month:
+  # heroku ps:scale worker=1
 end
